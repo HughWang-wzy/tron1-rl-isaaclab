04d77b08bb79ff8e6fdbdda0866e2a8b410c70e6
HughWang-wzy <2830794546@qq.com>
2026-03-12 17:36:00 +0800

    Stop tracking play.sh and train.sh

--- train.sh
+++
@@ -1,8 +0,0 @@
-# python scripts/rsl_rl/train.py --headless --experiment_name wf_tron_1a_flat --resume true --checkpoint model_3000.pt --load_run 2026-01-13_16-46-43 --task=Isaac-Limx-WF-Blind-Flat-v0
-
-# python scripts/rsl_rl/train.py --headless --task=Isaac-Limx-WF-Blind-Flat-v0
-
-# python scripts/rsl_rl/train.py --headless --task=Isaac-Limx-WF --num_envs 512
-
-# python scripts/rsl_rl/train.py --headless --task=Isaac-Limx-WF-Jump-Flat-v0 --num_envs 128
-python scripts/rsl_rl/train.py --headless --task=Isaac-Limx-WF-Jump-Flat-v0 --num_envs 4096
